f1265cb2f0117d944a5f67bb35b97381369ccc47
Nguyễn Trần Công Thắng <ntcongthang@gmail.com>
2017-11-07 21:14:21 +0700

    add LearnProject

---
+++ LearnProject/MyPlayground.playground/Contents.swift
@@ -0,0 +1,162 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//
+//var bien1:Int = 5
+//var bien2:String = "Thang"
+//let CMND:Int = 312239285
+//
+//let giaTri1:Int = 3
+//let GiaTri2:Int = 30
+//
+//if giaTri1 < GiaTri2 {
+//    print("gia tri 1 be hon gia tri 2")
+//}
+//
+//if giaTri1 < GiaTri2 {
+//    print("Gia tri 1 be hon gia tri 2")
+//}
+//else {
+//    print("Gia tri 1 lon hon")
+//}
+//
+//switch giaTri1 {
+//case 1 :
+//    print("Lon hon 1")
+//    break
+//case 2 where giaTri1 < GiaTri2:
+//    print("2 be hon")
+//    break
+//default:
+//    print("Bang nhau")
+//}
+// var giatriWhile = 0
+
+//while giaTri1 < 10 {
+//  print(giatriWhile)
+//    giatriWhile = giaTri1 + 1
+//}
+
+
+//var a:Int = 0
+//while a < 10 {
+//    print(a)
+//    a = a + 1
+//}
+
+//for i in 1..<<>10 {
+//    print(i)
+//}
+
+//var MangA:[String] = ["thang", "phuong"]
+//MangA.count
+//MangA[0]
+//MangA[1]
+
+//for i in 1..<100{
+//    print(i)
+//}
+
+//var a:Int = 4
+//var b:Int = 9
+//
+//for c in a..<<>b{
+//    print(c)
+//}
+
+//var Mang1:Array<Int> = [1, 2, 3, 4]
+//Mang1.append(45)
+//Mang1.count
+//Mang1[3]
+
+//var a:Int!
+//var b:Int?
+//
+//print(a)
+//
+//struct congnuoi{
+//    var ten:String?
+//    var cmnd:String!
+//}
+
+//var a:String?
+//    a = "abc"
+//    print(a!)
+
+var name:String = "Thang Nguyen"
+print(name)
+
+var age:Int = 10
+print(age)
+
+var latitude:Double = 36.234234
+var longtitude:Float = -86.7777980
+
+
+var b = 10
+b += 10
+b -= 10
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+var age2 = 25
+print("You are \(age2) years old. In another \(age2) years you will be \(age2 * 2).")
+
+var evenNumbers = [2, 4, 6, 8]
+var songs = ["Shake it Off", "You Belong with Me", "Back to December"]
+songs[0]
+songs[1]
+songs[2]
+type(of: songs)
+var songs2: [Any] = ["Shake it Off", "You Belong with Me", "Back to December", 3]
+
+
+var person = ["first": "thang", "middle": "cong", "last": "Tran"]
+person["first"]
+person["last"]
+
+var ten = person["last"]
+
+//var action: String
+//var person2 = "thang"
+//
+//if person == "thang"{
+//    action = true
+//}
+
+//
+//var action: String
+//var person = "hater"
+//
+//if person == "hater"{
+//    action = "hate"
+//} else if person == "player"{
+//    action = "play"
+//} else{
+//    action = "cruise"
+//}
+
+
+var action: String
+var stayOutTooLate = true
+var nothingInBrain = true
+
+//if stayOutTooLate && nothingInBrain {
+//    action = "cruise"
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
