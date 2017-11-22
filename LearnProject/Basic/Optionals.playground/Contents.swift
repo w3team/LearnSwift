464a426f619374cfeef989f70a67c1a496010c9c
Nguyễn Trần Công Thắng <ntcongthang@gmail.com>
2017-11-22 21:50:51 +0700

    Add code

--- LearnProject/Basic/Optionals.playground/Contents.swift
+++ LearnProject/Basic/Optionals.playground/Contents.swift
@@ -6,27 +6,58 @@ import UIKit
 //var message:String = "I’m learning Swift" //ok
 //message = nil //error
 
-class Message {
-    var mes1: String = "I’m learning Swift" //ok
-    var mes2: String? // compile-time error
-}
-
-
-func findStockCode(company:String) -> String?{
-    if company == "Apple"{
-        return "APPL"
-    }
-    else if company == "Google"{
-        return "GOOG"
-    }
-    nil
-}
-
-var stockCode:String? = findStockCode(company: "Facebook")
-let text:String = "Stock code -"
+//class Message {
+//    var mes1: String = "I’m learning Swift" //ok
+//    var mes2: String? // compile-time error
+//}
+//
+//
+//func findStockCode(company:String) -> String?{
+//    if company == "Apple"{
+//        return "APPL"
+//    }
+//    else if company == "Google"{
+//        return "GOOG"
+//    }
+//    nil
+//}
+//
+//var stockCode:String? = findStockCode(company: "Facebook")
+//let text:String = "Stock code -"
 //let message = text + stockCode // compile-time error
 //print(message)
 
 //sua loi tren
+
+
+
+//func getHaterStatus() -> String? {
+//    return "Hate"
+//}
+
+//--------------------------------------------------------------quan trong
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+//var status: String?
+//var status = getHaterStatus(weather: "rainy")
+
+
+func takeHaterAction(status: String){
+    if status == "Hate"{
+        print("Hating")
+    }
+}
+
+takeHaterAction(status: "Hate")
+
+if let haterStatus = getHaterStatus(weather: "rainy") {
+    takeHaterAction(status: haterStatus)
+}
 
 
