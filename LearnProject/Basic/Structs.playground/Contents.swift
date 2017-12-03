e2f37cd0a288126547136f14c5d3b9e9e12ccd3c
Nguyễn Trần Công Thắng <ntcongthang@gmail.com>
2017-12-03 15:59:04 +0700

    Add struct

--- LearnProject/Basic/Structs.playground/Contents.swift
+++ LearnProject/Basic/Structs.playground/Contents.swift
@@ -1,5 +1,35 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//
+//struct person{
+//    var clothes: String
+//    var shoes: String
+//}
+//
+//let Taylor = person(clothes: "T-Shirt", shoes: "Nike")
+//let other = person(clothes: "shorts kirts", shoes: "Adidass")
+//
+//print(Taylor.clothes)
+//print(other.shoes)
+//
+//var taylorcoppy = Taylor
+//taylorcoppy.shoes = "flip flops"
+//print(Taylor)
+//print(taylorcoppy)
 
-var str = "Hello, playground"
+struct persons{
+    var clothes: String
+    var shoes: String
+    
+    func describe(){
+        print("I like wearning \(clothes) with \(shoes)")
+    }
+}
+let Thang = persons(clothes: "jeans", shoes: "nike")
+ print(Thang.describe())
+
+
+
+
+
